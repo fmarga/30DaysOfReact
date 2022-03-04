5158863aa7c2bef65d0a102ffb43f6566f1e483f
fmarga <marga.fmartins@gmail.com>
2022-03-04 15:29:58 -0300

    day07

--- README.md
+++ README.md
@@ -7,5 +7,12 @@
 3. Day 03 - [Setting up](https://github.com/fmarga/30DaysOfReact/tree/master/day03)
 4. Day 04 - [Components](https://github.com/fmarga/30DaysOfReact/tree/master/day04)
 5. Day 05 - [Props](https://github.com/fmarga/30DaysOfReact/tree/master/day05)
-6. Day 06 - List, Map, and Keys
+6. Day 06 - [List, Map, and Keys](https://github.com/fmarga/30DaysOfReact/tree/master/day06/day05app)
 7. Day 07 - Class components
+8. Day 08 - States
+9. Day 09 - Conditional rendering
+10. Day 10 - React Project Folder Structure
+11. Day 11 - Events
+12. Day 12 - Forms
+13. Day 13 - Controlled and Uncontrolled component
+14. Day 14 - Component Life Cycles
---
+++ day07/script.js
@@ -0,0 +1,28 @@
+/*
+        How do you write a pure JavaScript function
+
+
+        What is inheritance and how do you make a child from a parent class?
+
+
+        What is class based React component ?
+        it's a component made using javaScript class and its inherits from react component
+
+
+        What is the difference between functional React component and class based React component ?
+        the functional component couldn't use state and react life cycle, but the newest versions of react made functional and class based component generate the same result
+
+
+        When do we need to use class based components instead of functional components?
+        when a statefull component is required
+
+
+        Which type of component do use most frequently ? functional or class-based component
+        functional component is more used than class based component
+
+
+        What is React life cycle ? (not covered yet) ?
+
+
+        What is state in React ? (not covered yet)
+*/
